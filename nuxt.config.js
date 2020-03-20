5b93907d072faf7e9000f1042de06eafbda9b694
Michael Karanja <mikekaranja63@gmail.com>
2020-03-21 02:26:39 +0300

    fixed config errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,8 +99,8 @@ export default {
   // sentry
   sentry: {
     dsn: 'https://f7e8c12f229545fa87a3f627f38ef57d@sentry.io/5169439', // Enter your project's DSN here
-    config: {}, // Additional config
-  }
+    config: {} // Additional config
+  },
   // manifest
   manifest: {
     name: 'E-merse',
